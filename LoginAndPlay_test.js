3236bee56570849581d31eec06c2f08cb1edbaa7
saranya-code <62154238+saranya-code@users.noreply.github.com>
2021-01-18 13:10:33 -0600

    Puppeteer-Initial set up

---
+++ LoginAndPlay_test.js
@@ -0,0 +1,18 @@
+Feature('LoginNavigate1');
+
+Scenario('Login to Demo Site', ({ I }) => {
+    I.amOnPage('/Login.html')
+    I.fillField("UserName","admin")
+    I.fillField("Password","Password")
+    I.click("//input[@type='submit']")
+    I.waitForElement({xpath: "//input[@name='Initial']"})
+    I.saveScreenshot("loggedIn.jpg")
+
+    I.selectOption({css: 'select[name=TitleId]'},"Mr.")
+    //pause()
+    I.checkOption("Hindi")
+    I.fillField("FirstName","saran")
+
+
+
+});
---
+++ codecept.conf.js
@@ -0,0 +1,41 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  multiple: {
+  basic: {
+    browsers: ["chrome", "firefox"]
+  }
+},
+
+  helpers: {
+    Puppeteer: {
+      url: 'https://demosite.executeautomation.com/',
+      show: true,
+      windowSize: '1200x900'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'codeceptjsdemo',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
